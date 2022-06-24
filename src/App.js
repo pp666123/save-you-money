1921d3d7ebafb7eb62e371821f626ce60a0a19d6
pp666123 <pp666123@gmail.com>
2022-06-24 18:27:02 +0800

    try

--- src/App.js
+++ src/App.js
@@ -1,47 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import { Grid } from '@mui/material';
-import Button from '@mui/material/Button';
-import { gridSpacing } from './store/constant';
+import { useSelector } from 'react-redux';
 
-function App() {
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
+
+// routing
+import Routes from 'routes';
+
+// defaultTheme
+import themes from 'themes';
+
+// project imports
+import NavigationScroll from 'layout/NavigationScroll';
+
+// ==============================|| APP ||============================== //
+
+const App = () => {
+    const customization = useSelector((state) => state.customization);
+
     return (
-        <Grid container spacing={gridSpacing}>
-            <Grid item xs={12}>
-                <Grid container spacing={gridSpacing}>
-                    <Grid item lg={4} md={6} sm={6} xs={12}>
-                        {/* <EarningCard isLoading={isLoading} /> */}
-                    </Grid>
-                    <Grid item lg={4} md={6} sm={6} xs={12}>
-                        <Button variant="contained">你好，世界</Button>
-                        {/* <TotalOrderLineChartCard isLoading={isLoading} /> */}
-                    </Grid>
-                    <Grid item lg={4} md={12} sm={12} xs={12}>
-                        <Grid container spacing={gridSpacing}>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                {/* <TotalIncomeDarkCard isLoading={isLoading} /> */}
-                            </Grid>
-                            <Grid item sm={6} xs={12} md={6} lg={12}>
-                                {/* <TotalIncomeLightCard isLoading={isLoading} /> */}
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item xs={12}>
-                <Grid container spacing={gridSpacing}>
-                    <Grid item xs={12} md={8}>
-                        {/* <TotalGrowthBarChart isLoading={isLoading} /> */}
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        {/* <PopularCard isLoading={isLoading} /> */}
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Grid>
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={themes(customization)}>
+                <CssBaseline />
+                <NavigationScroll>
+                    <Routes />
+                </NavigationScroll>
+            </ThemeProvider>
+        </StyledEngineProvider>
     );
-}
+};
 
 export default App;
